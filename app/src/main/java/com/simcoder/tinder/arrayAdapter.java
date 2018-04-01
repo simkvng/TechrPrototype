d7e2d6f7f8b832bc67520a17c153709e7c0af200
漢字 <emulateking@gmail.com>
2018-03-31 20:53:20 -0600

    H
    H

--- app/src/main/java/com/simcoder/tinder/arrayAdapter.java
+++ app/src/main/java/com/simcoder/tinder/arrayAdapter.java
@@ -1,7 +1,14 @@
 package com.simcoder.tinder;
 
+import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by natha on 3/30/2018.
@@ -9,8 +16,8 @@ import android.widget.ArrayAdapter;
 
 public class arrayAdapter extends ArrayAdapter<Cards>{
     Context context;
-    public_arrayAdapter(Context context, int resourceID, List<Cards> items){
-        super(context, resourceID, items);
+    public arrayAdapter(Context context, int resourceID, List<Cards> items){
+        Super(context, resourceID, items);
     }
     public View getView(int position, View convertView, ViewGroup parent){
         Cards cardsItem = getItem(position);
@@ -23,7 +30,7 @@ public class arrayAdapter extends ArrayAdapter<Cards>{
         TextView name = (TextView) convertView.findViewById(R.id.name);
         ImageView image = (ImageView) convertView.findViewById(R.id.image);
 
-        name.setTExt(cardsItem.getName());
+        name.setText(cardsItem.getName());
         image.setImageResource(R.mipmap.ic_launcher);
 
         return convertView;
