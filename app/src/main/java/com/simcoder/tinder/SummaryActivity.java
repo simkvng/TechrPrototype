d554af4b1dddb465d412d9190900d549b5c53b73
Nathan Chua <nchua235@mtroyal.ca>
2018-04-03 20:52:20 -0600

    more documentation

--- app/src/main/java/com/simcoder/tinder/SummaryActivity.java
+++ app/src/main/java/com/simcoder/tinder/SummaryActivity.java
@@ -3,6 +3,10 @@ package com.simcoder.tinder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/*
+    this class is to be implemented in future versions. currently not implemented because of
+    problems we ran into trying to use the class.
+ */
 public class SummaryActivity extends AppCompatActivity {
 
 
